a1da87c957521b3949289634cd812e65d5203a06
Lewous <lmnsodv@gmail.com>
2014-12-21 13:48:50 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
 ## This functions is designed to caching the inverse of a matrix
-## so that it will not be computed repeatedly when needed to avoid a costly computation.
+## so that it will not be computed repeatedly when accessed again to  avoid a costly computation.
 
-## to creates a special "matrix" object that can cache its inverse.
+## This function will create a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x) {
-       
+              ## to check simply whether x can be inversed 
        if(!is.matrix(x)) {return(message("not a matrix"))}
        if(!ncol(x)==nrow(x)) {return(message("not a square"))}
        
@@ -12,24 +12,24 @@ makeCacheMatrix <- function(x) {
               x <<- y
               s <<-NULL
        }
-       get <-function()  {x}            ## Those functions will be used in cacheSolve(x) below.
+       get <-function()  {x}            ## Those functions will be used in the cacheSolve() below.
        setmatrix <-function(solve) {s <<-solve}
        getmatrix <-function() {s}
        list(set = set, get = get, 
             setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
-## to compute the inverse of the special "matrix" returned by makeCacheMatrix above. 
-## If the inverse is an old one, the cachesolve would retrieve result directy.
+## This function will compute the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## Inter alia, if the matrix is an old one, the cacheSolve() can retrieve the result directy.
 cacheSolve <-function (x, ...) {
        s <- x$getmatrix()
-       if(!is.null(s)) {
+       if(!is.null(s)) {           ## to check whether x has been caculatetd
               message("getting cached data")
-              return(s)
+              return(s)            ## If it has, give the inverse dircetly; skip all beneath.
        }
-       data <-x$get()       ## If x$getmatrix() get NULL, than we caculate it.
+       data <-x$get()       ## If x is a new one(x$getmatrix=NULL), than we caculate it,
        s <-solve(data, ...)
-       x$setmatrix(s)       ## to store the result so that can be recalled.
+       x$setmatrix(s)       ## and store the result so that can be recalled.
        s             ## to return a matrix that is the inverse of "x"
 }
 
